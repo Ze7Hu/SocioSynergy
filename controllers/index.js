b072d8342dfa833913d969d5bdfe3e6299a5279b
Carmela <carmela881@outlook.com>
2023-05-06 10:14:11 +0100

    added index route to define various routes

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+// Defines various routes
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const profileRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/profile', profileRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
