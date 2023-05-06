a636f23509e6c2a52451f1b391f2060218e3a974
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-05-06 16:58:38 +0100

    imgs moved, .js scripts to handle login and signup forms

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if(email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if(response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
