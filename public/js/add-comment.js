64cc6381f773559224328f20e21f7c13420be4df
Carmela <carmela881@outlook.com>
2023-05-11 09:09:01 +0100

    add-comments.js added

---
+++ public/js/add-comment.js
@@ -0,0 +1,34 @@
+// Add comments to posts 
+
+async function commentFormHandler(event) {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('textarea#comment-textarea').value.trim();
+  
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          post_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+        document.querySelector('#add-comment-form').style.display = "block";
+      }
+    }
+  }
+  
+  document.querySelector('#add-comment-form').addEventListener('submit', commentFormHandler);
+  
