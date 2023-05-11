26bde5a93eac639922bed56e0d57c6b4b8380a96
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-05-11 17:24:20 +0100

    helper function to format date/time and display it on the page

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${
+        new Date(date).getFullYear()
+      }`;
+    },
+  };
+  
