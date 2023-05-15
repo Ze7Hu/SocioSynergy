8c12cf5814e62b9a5284a3bb459095cfd7bb4122
Carmela <carmela881@outlook.com>
2023-05-15 17:23:53 +0100

    date and time to posts in profile

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,24 @@
-module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-      return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${
-        new Date(date).getFullYear()
-      }`;
-    },
+// helpers.js
+const handlebars = require('handlebars');
+
+handlebars.registerHelper('format_date', (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   };
-  
+
+  return new Date(date).toLocaleDateString('en-US', options);
+});
+
+handlebars.registerHelper('format_time', (date) => {
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+
+  return new Date(date).toLocaleTimeString('en-US', options);
+});
+
+module.exports = handlebars;
