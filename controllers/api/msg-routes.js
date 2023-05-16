0ca8d1962322d6e27e809c0cf7027f9eba0c3ccf
Hodan Ibrahim <hodanibrahim@Hodans-MacBook-Air.local>
2023-05-16 20:03:09 +0100

    amended comments msg-routes

--- controllers/api/msg-routes.js
+++ controllers/api/msg-routes.js
@@ -1,48 +1,49 @@
-const router = require('express').Router();
-const withAuth = require('../../utils/auth');
-const { User, Message } = require('../../models');
+const router = require("express").Router();
+const withAuth = require("../../utils/auth");
+const { User, Message } = require("../../models");
 
-// Create new message
-router.post('/', (req, res) => {
-    Message.create({
-        msg_content: req.body.msg_content,
-        recipient_id: req.body.recipient_id,
-        sender_id: req.body.sender_id,
-    })
-    .then(msgData => res.json(msgData))
-    .catch(error => {
-        res.status(500).json(error);
-    })
+router.post("/", (req, res) => {
+  Message.create({
+    msg_content: req.body.msg_content,
+    recipient_id: req.body.recipient_id,
+    sender_id: req.body.sender_id,
+  })
+    .then((msgData) => res.json(msgData))
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
-// Get all user messages
-router.get('/:id', (req, res) => {
-    Message.findAll({
-        where: {
-            recipient_id: req.params.id,
-        },
-        order: [
-            ['created_at', 'DESC']
-        ],
-        include: [{
-            model: User,
-            as: 'sender',
-            attributes: ['id', 'username']
-        },
-        {
-            model: User,
-            as: 'recipient',
-            attributes: ['id', 'username']
-        }]
-    }).then(msgData => {
-        if(!msgData) {
-            res.status(404).json({ message: 'No messages!'});
-            return;
-        }
-        const msgs = msgData.map((msg) => msg.get({ plain: true }))
-        console.log(msgs)
-        res.render('messages', { msgs, logged_in: req.session.logged_in, user_id: req.session.user_id })
-
-    })
-})
+router.get("/:id", (req, res) => {
+  Message.findAll({
+    where: {
+      recipient_id: req.params.id,
+    },
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        as: "sender",
+        attributes: ["id", "username"],
+      },
+      {
+        model: User,
+        as: "recipient",
+        attributes: ["id", "username"],
+      },
+    ],
+  }).then((msgData) => {
+    if (!msgData) {
+      res.status(404).json({ message: "No messages!" });
+      return;
+    }
+    const msgs = msgData.map((msg) => msg.get({ plain: true }));
+    console.log(msgs);
+    res.render("messages", {
+      msgs,
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id,
+    });
+  });
+});
 module.exports = router;
