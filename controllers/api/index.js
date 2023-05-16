2530e4b0be4c9358765f9242634562dc41f33bb5
Hodan Ibrahim <hodanibrahim@Hodans-MacBook-Air.local>
2023-05-16 20:01:13 +0100

    comments amended index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,12 @@
-// Import Express and routes modules
-const router = require ('express').Router();
-const userRoutes = require('./user-routes.js');
-const postRoutes = require ('./post-routes.js');
-const commentRoutes = require ('./comment-route.js');
-const msgRoutes = require('./msg-routes.js');
+const router = require("express").Router();
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes.js");
+const commentRoutes = require("./comment-route.js");
+const msgRoutes = require("./msg-routes.js");
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
-router.use('/msg', msgRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
+router.use("/msg", msgRoutes);
 
 module.exports = router;
